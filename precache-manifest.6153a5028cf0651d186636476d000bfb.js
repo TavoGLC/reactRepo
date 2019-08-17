ebed2e3015ecf0579a60bdb7e28ecac147def8d2
Octavio Gonzalez-Lugo <ogonzalezl@outlook.com>
2019-08-17 18:43:25 -0500

    Updates

---
+++ precache-manifest.6153a5028cf0651d186636476d000bfb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d8ea603c57eb333faa321e5d0ad20b",
+    "url": "/reactRepo/index.html"
+  },
+  {
+    "revision": "612cd00a908ae682f7ff",
+    "url": "/reactRepo/static/js/2.6bd69417.chunk.js"
+  },
+  {
+    "revision": "2dd146d61dde5afdc115",
+    "url": "/reactRepo/static/js/main.17ebfbc6.chunk.js"
+  },
+  {
+    "revision": "cdb836da64d5f07bd745",
+    "url": "/reactRepo/static/js/runtime~main.53220a4e.js"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
-import Timer from './components/timer'
-
-const element = <h1>Stop Watch App</h1>;
-
-ReactDOM.render(element,document.getElementById('Welcome'))
-ReactDOM.render(<Timer/>,document.getElementById('Timer'))
